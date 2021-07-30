2799107c8c20c7b103d6af361e762f1942b86aef
AnushikaNethsara <anushikanethsara@gmail.com>
2021-07-30 05:43:03 +0530

    training view added

--- lib/Interfaces/TrainingView.dart
+++ lib/Interfaces/TrainingView.dart
@@ -13,3 +13,96 @@ class _SessionSingleState extends State<SessionSingle> {
     return Container();
   }
 }
+
+class TrainingView extends StatelessWidget {
+  final assetPath, cookieprice, cookiename;
+
+  TrainingView({this.assetPath, this.cookieprice, this.cookiename});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Training View',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                color: Color(0xFF545D68))),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: ListView(
+        children: [
+          SizedBox(height: 15.0),
+            // SizedBox(height: 15.0),
+            // Hero(
+            //   tag: assetPath,
+            //   child: Image.asset(assetPath,
+            //   height: 150.0,
+            //   width: 100.0,
+            //   fit: BoxFit.contain
+            //   )
+            // ),
+            SizedBox(height: 20.0),
+            SizedBox(height: 10.0),
+            Center(
+              child: Text("Title",
+                  style: TextStyle(
+                      color: Color(0xFF575E67),
+                      fontFamily: 'Varela',
+                      fontSize: 24.0)),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 50.0,
+                child: Text('In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 16.0,
+                      color: Color(0xFFB4B8B9))
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: Color(0xFFF17532)
+                ),
+                child: Center(
+                  child: Text('View',
+                    style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                ),
+                  )
+                )
+              )
+            )
+        ]
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
